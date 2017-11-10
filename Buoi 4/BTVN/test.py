97523971a0e42fa95aa5beda6df5d195c06c2efc
Minh Thanh <minhthanh@Minhs-MacBook-Pro.local>
2017-11-10 16:30:08 +0700

    HW

--- Buoi 4/BTVN/test.py
+++
@@ -1,8 +0,0 @@
-counts = {}
-data = "ThiS is String with Upper and lower case Letters"
-for i in data.lower():
-    counts[i] = counts.get(i, 0) + 1
-item = list(counts.items())
-item.sort()
-for i in range(1, len(item)):
-    print(*item[i])
